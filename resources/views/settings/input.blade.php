1369352bded683cda4fb2af50c887ac1add3d65f
Simon Flachs <simon-flachs@hotmail.com>
2015-04-30 19:54:24 +0200

    layout tweaks

--- resources/views/settings/input.blade.php
+++ resources/views/settings/input.blade.php
@@ -1,58 +1,109 @@
 @extends('app')
 
 @section('content')
+
+    <div class="jumbotron">
+        <div class="container">
+            <h1 class="jumbotron__header">You have arrived</h1>
+            <p class="jumbotron__body">
+                adsfasdf
+                asdfasdf
+            </p>
+        </div>
+    </div>
+
+
+
     <div class="container">
         <div class="row">
-            <div class="col-md-10 col-md-offset-1">
+            <div class="col-md-12">
                 <div class="panel panel-default">
                     <div class="panel-heading">Home</div>
 
                     <div class="panel-body">
-                        You are logged in!
+                        {!! Form::open(array('url' => 'settings/inventory')) !!}
+
+                        {!! Form::label('globalLimit', 'Inventory Limit') !!}
+                        {!! Form::text('globalLimit', $setting->globalLimit) !!}
+                        {!! Form::hidden('id', $setting->id) !!}
+                        {!! Form::submit('save') !!}
+
+                        {!! Form::close() !!}
+
+                        <hr/>
+
+                        {!! Form::open(array('url' => 'settings/inventory/search')) !!}
+
+                        {!! Form::label('productTitle', 'Search by product title') !!}
+                        {!! Form::text('productTitle') !!}
+                        {!! Form::submit('save') !!}
+
+                        {!! Form::close() !!}
+
+                        @if (isset($matches))
+                            @foreach($matches as $product)
+
+
+                                <h2>{{$product->title}}</h2>
+
+                                <table class="table table-striped table-bordered table-hover">
+                                    <thead>
+                                    <tr>
+                                        <th></th>
+                                        <th>Limit</th>
+                                        <th></th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+
+                                    @foreach($product->variants as $variant)
+
+
+                                        {!! Form::open(array('url' => 'settings/inventory/limit')) !!}
+
+                                        <tr>
+                                            <td><p>{{$variant->title}}</p></td>
+                                            <td>
+                                                {!! Form::text('individualLimit', $variant->inventory_limit) !!}
+                                                {!! Form::hidden('variantId', $variant->id) !!}
+                                                {!! Form::hidden('productId', $product->id) !!}
+                                            </td>
+                                            <td class="text-right">{!! Form::submit('save') !!}</td>
+                                        </tr>
+
+
+
+
+                                        {!! Form::close() !!}
+                                    @endforeach
+
+
+                                    <tbody>
+                                </table>
+
+
+
+
+
+
+                            @endforeach
+                        @endif
                     </div>
                 </div>
             </div>
+
         </div>
     </div>
 
-    {!! Form::open(array('url' => 'settings/inventory')) !!}
-
-        {!! Form::label('globalLimit', 'Inventory Limit') !!}
-        {!! Form::text('globalLimit', $setting->globalLimit) !!}
-        {!! Form::hidden('id', $setting->id) !!}
-        {!! Form::submit('save') !!}
-
-    {!! Form::close() !!}
-
-    {!! Form::open(array('url' => 'settings/inventory/search')) !!}
-
-        {!! Form::label('productTitle', 'Search by product title') !!}
-        {!! Form::text('productTitle') !!}
-        {!! Form::submit('save') !!}
-
-    {!! Form::close() !!}
 
 
-    @if (isset($matches))
-        @foreach($matches as $product)
 
 
-            <h2>{{$product->title}}</h2>
-            @foreach($product->variants as $variant)
-                <p>{{$variant->title}}</p>
-
-                {!! Form::open(array('url' => 'settings/inventory/limit')) !!}
-
-                {!! Form::label('individualLimit', 'Set Limit') !!}
-                {!! Form::text('individualLimit', $variant->inventory_limit) !!}
-                {!! Form::hidden('variantId', $variant->id) !!}
-                {!! Form::hidden('productId', $product->id) !!}
-                {!! Form::submit('save') !!}
-
-                {!! Form::close() !!}
-            @endforeach
-        @endforeach
-    @endif
 
 
 @endsection
+
+
+
+
+
