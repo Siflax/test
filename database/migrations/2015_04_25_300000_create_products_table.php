1d17c1d92069dc326d00d79c3396f486fab0350a
Simon Flachs <simon-flachs@hotmail.com>
2015-06-28 08:14:16 +0200

    changed prod id to big integer

--- database/migrations/2015_04_25_300000_create_products_table.php
+++ database/migrations/2015_04_25_300000_create_products_table.php
@@ -14,7 +14,7 @@ class CreateProductsTable extends Migration {
     {
         Schema::create('products', function(Blueprint $table)
         {
-            $table->integer('id');
+            $table->bigInteger('id');
             $table->integer('shop_id')->unsigned();
             $table->integer('inventory_limit');
             $table->boolean('track')->default(True);
