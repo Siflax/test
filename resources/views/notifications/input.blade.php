2ee0f635423fd1aefca84b32d8bf5be15c6b559b
Simon Flachs <simon-flachs@hotmail.com>
2015-06-30 15:57:39 +0200

    changed layout of notifications

--- resources/views/notifications/input.blade.php
+++ resources/views/notifications/input.blade.php
@@ -7,83 +7,90 @@
             <div class="col-md-12">
 
                 <div class="panel panel-default">
-                    <div class="panel-heading">Frequency</div>
+                    <div class="panel-heading">Notifications</div>
 
                     <div class="panel-body">
-                        <div class="well">
-                            Lorem ipsum dolor sit amet, natum inermis pericula sed ei, cu malis legere phaedrum nec. Qui ex decore honestatis, ex magna utinam regione qui. Adhuc eleifend appellantur id mel. No mundi ceteros his, nostrum philosophia qui in. Sententiae consequuntur quo eu, nam veri erant nominavi at, eam solum voluptua contentiones id. Nec et ancillae hendrerit, usu commodo iuvaret adolescens ut, paulo fabulas per ex.
+
+                        <div class="col-md-12">
+                            <div class="well">
+                                Lorem ipsum dolor sit amet, natum inermis pericula sed ei, cu malis legere phaedrum nec. Qui ex decore honestatis, ex magna utinam regione qui. Adhuc eleifend appellantur id mel. No mundi ceteros his, nostrum philosophia qui in. Sententiae consequuntur quo eu, nam veri erant nominavi at, eam solum voluptua contentiones id. Nec et ancillae hendrerit, usu commodo iuvaret adolescens ut, paulo fabulas per ex.
+                            </div>
                         </div>
 
-                        {!! Form::open(array('route' => 'saveFrequency')) !!}
+                        <div class="col-md-6">
+                            
+                            {!! Form::open(array('route' => 'saveFrequency')) !!}
 
-                        <div class="form-group">
+                            <div class="form-group">
+                                <div class="input-group">
+                                    {!! Form::label('frequency', 'Daily') !!}
+                                    {!! Form::radio('frequency', 'Daily', $settings->frequencyIsDaily(), ['class' => 'form-control']) !!}
+                                </div>
+                                <div class="input-group">
+                                    {!! Form::label('frequency', 'Weekly') !!}
+                                    {!! Form::radio('frequency', 'Weekly', $settings->frequencyIsWeekly(), ['class' => 'form-control']) !!}
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                {!! Form::submit('Save', ['class'=> 'btn btn-primary']) !!}
+                            </div>
+                            {!! Form::close() !!}
 
-                        {!! Form::label('frequency', 'Daily') !!}
-                        {!! Form::radio('frequency', 'Daily', $settings->frequencyIsDaily(), ['class' => 'form-control']) !!}
-
-                        {!! Form::label('frequency', 'Weekly') !!}
-                        {!! Form::radio('frequency', 'Weekly', $settings->frequencyIsWeekly(), ['class' => 'form-control']) !!}
                         </div>
-                        <div class="form-group">
-                            {!! Form::submit('Save', ['class'=> 'btn btn-primary']) !!}
+
+                        <div class="col-md-6">
+
+
+                            <div class="form-group">
+
+                                {!! Form::open(array('url' => '/notifications/email')) !!}
+
+                                <div class="input-group">
+
+                                    {!! Form::text('emailAddress', null, ['class' => 'form-control', 'placeholder' => 'Add Email Address']) !!}
+
+                                <span class="input-group-btn">
+                                    {!! Form::submit('Add', ['class'=> 'btn btn-primary']) !!}
+                                </span>
+                                </div>
+                                {!! Form::close() !!}
+                            </div>
+
+                            <table class="table table-striped table-bordered table-hover">
+                                <thead>
+                                <tr>
+                                    <th>Emails</th>
+                                    <th></th>
+                                </tr>
+                                </thead>
+                                <tbody>
+
+                                @if(isset($emails))
+                                    @foreach($emails as $email)
+
+                                        {!! Form::open(array('route' => ['deleteEmail', $email->id], 'method' => 'Delete')) !!}
+                                        <tr>
+                                            <td>{{$email->address}}</td>
+                                            <td class="text-right">{!! Form::submit('Remove', ['class'=> 'btn btn-danger']) !!}</td>
+                                        </tr>
+                                        {!! Form::close() !!}
+
+                                    @endforeach
+
+                                    <?php echo $emails->render(); ?>
+                                @endif
+
+
+                                <tbody>
+                            </table>
+
                         </div>
-                        {!! Form::close() !!}
 
                     </div>
                 </div>
             </div>
 
-<div class="col-md-12">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Emails</div>
 
-                    <div class="panel-body">
-                        <div class="well">
-                            Lorem ipsum dolor sit amet, natum inermis pericula sed ei, cu malis legere phaedrum nec. Qui ex decore honestatis, ex magna utinam regione qui. Adhuc eleifend appellantur id mel. No mundi ceteros his, nostrum philosophia qui in. Sententiae consequuntur quo eu, nam veri erant nominavi at, eam solum voluptua contentiones id. Nec et ancillae hendrerit, usu commodo iuvaret adolescens ut, paulo fabulas per ex.
-                        </div>
-
-                        {!! Form::open(array('url' => '/notifications/email')) !!}
-                            <div class="form-group">
-                            {!! Form::label('emailAddress', 'Add Email') !!}
-                            {!! Form::text('emailAddress', null, ['class' => 'form-control']) !!}
-                            </div>
-                            <div class="form-group">
-                            {!! Form::submit('Add', ['class'=> 'btn btn-primary']) !!}
-                            </div>
-                        {!! Form::close() !!}
-
-                        <table class="table table-striped table-bordered table-hover">
-                            <thead>
-                            <tr>
-                                <th>Emails</th>
-                                <th></th>
-                            </tr>
-                            </thead>
-                            <tbody>
-
-                            @if(isset($emails))
-                                @foreach($emails as $email)
-
-                                    {!! Form::open(array('route' => ['deleteEmail', $email->id], 'method' => 'Delete')) !!}
-                                    <tr>
-                                        <td>{{$email->address}}</td>
-                                        <td class="text-right">{!! Form::submit('Remove', ['class'=> 'btn btn-primary']) !!}</td>
-                                    </tr>
-                                    {!! Form::close() !!}
-
-                                @endforeach
-
-                                <?php echo $emails->render(); ?>
-                            @endif
-
-
-                            <tbody>
-                        </table>
-
-                    </div>
-                </div>
-    </div>
-            </div>
 
         </div>
     </div>
